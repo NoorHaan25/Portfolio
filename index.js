7ea57a88bdc2de07d3d342bfd1470d2e8b3f65f1
NoorHaan25 <nourhantaha588@gmail.com>
2024-07-28 15:38:19 +0300

    Add files and structure for portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+const toTop= document.querySelector('.to-top');
+window.addEventListener('scroll', function(){
+  this.scrollY > 100 ? toTop.classList.add('active') : toTop.classList.remove('active');
+});
+toTop.addEventListener('click', function(){
+  window.scrollTo({
+    top:0
+  })
+})
